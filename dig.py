e77abbe37190d1b2844a44136a6174b314ca2b3a
Peter McGarrigle <mcgarrigle@github.com>
2023-01-20 13:28:19 +0000

    separated dig class into module

--- dig.py
+++ dig.py
@@ -1,5 +1,3 @@
-import pprint
-
 class dig:
 
     def __init__(self, value):
@@ -21,38 +19,3 @@ class dig:
     @property
     def _value(self):
         return self.value
-
-# ----------------------------------------
-
-s = {
-   "a":1, 
-   "b": { "b1": 20, "b2": 21 },
-   "c": [ 30, 31, 32 ],
-   "d": { "d1": [ { "d11" : 41 }, { "d12": 42 } ] }
-}
-
-def example(s):
-  r = eval(s)
-  print(s, " ", r)
-
-pprint.pprint(s)
-o = dig(s)
-
-print(o.d.d1[0].d11)
-
-example("o.a")
-example("o.b.b1")
-example("o.b.b2")
-example("o.c")
-example("o.c._value")
-example("o.c[1]")
-example("o.d._value")
-example("o.d.d1")
-example("o.d.d1[0]")
-example("o.d.d1[0].d11")
-example("o.d.d1[1]")
-example("o.d.d1[1].d12")
-
-print("o.c")
-for i in o.c:
-    print("  ", i)
