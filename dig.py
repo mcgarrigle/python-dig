0020891ea64784306ecac94bbddddfe49006c8f9
Peter McGarrigle <mcgarrigle@github.com>
2023-01-20 13:22:56 +0000

    added _value @property

--- dig.py
+++ dig.py
@@ -6,27 +6,34 @@ class dig:
         self.value = value
 
     def __getattr__(self, name):
-        return self.__value(self.value[name])
+        return self.__dig_this(self.value[name])
 
     def __getitem__(self, index):
-        return self.__value(self.value[index])
+        return self.__dig_this(self.value[index])
 
-    def __value(self, value):
+    def __dig_this(self, value):
         if type(value) is dict:
             return dig(value)
         if type(value) is list:
-            return [ self.__value(i) for i in value ]
+            return dig(value)
         return value
 
-s = { "a":1, 
-      "b": { "b1": 20, "b2": 21 },
-      "c": [ 30, 31, 32 ],
-      "d": { "d1": [ { "d11" : 41 }, { "d12": 42 } ] }
-    }
+    @property
+    def _value(self):
+        return self.value
+
+# ----------------------------------------
+
+s = {
+   "a":1, 
+   "b": { "b1": 20, "b2": 21 },
+   "c": [ 30, 31, 32 ],
+   "d": { "d1": [ { "d11" : 41 }, { "d12": 42 } ] }
+}
 
 def example(s):
   r = eval(s)
-  print(s," ",r)
+  print(s, " ", r)
 
 pprint.pprint(s)
 o = dig(s)
@@ -36,7 +43,10 @@ print(o.d.d1[0].d11)
 example("o.a")
 example("o.b.b1")
 example("o.b.b2")
+example("o.c")
+example("o.c._value")
 example("o.c[1]")
+example("o.d._value")
 example("o.d.d1")
 example("o.d.d1[0]")
 example("o.d.d1[0].d11")
@@ -45,4 +55,4 @@ example("o.d.d1[1].d12")
 
 print("o.c")
 for i in o.c:
-    print(" ",i)
+    print("  ", i)
